5905ea41a2345d0e543d9ff5464e0981843f1dfa
Gyumin Sim <sim0629@gmail.com>
2015-10-01 22:24:58 +0900

    Transpose images

--- code_2009-11744/load_data.m
+++ code_2009-11744/load_data.m
@@ -1,7 +1,7 @@
 function [trainingData, testingData] = load_data(datasetPath)
   data = load(datasetPath);
-  trainingData.Images = data.trainingData.Images(:,1:10000);
+  trainingData.Images = data.trainingData.Images(:,1:10000)';
   trainingData.Labels = data.trainingData.Labels(1:10000,:);
-  testingData.Images = data.testingData.Images(:,1:2000);
+  testingData.Images = data.testingData.Images(:,1:2000)';
   testingData.Labels = data.testingData.Labels(1:2000,:);
 end

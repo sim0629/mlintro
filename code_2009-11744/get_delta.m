0a18648a31967cb84c57f73c06b9f77a96208cbd
Gyumin Sim <sim0629@gmail.com>
2015-10-04 03:11:23 +0900

    Print some information including the elapsed time

--- code_2009-11744/get_delta.m
+++ code_2009-11744/get_delta.m
@@ -1,16 +1,15 @@
-function [delta] = get_delta(results, params, hidden, kernel, images, labels)
+function [delta] = get_delta(etaM, etaV, results, params, hidden, kernel, images, labels)
   y = one_of_n(labels);
   w = reset_bias(params);
   common = ((y .- results) * w') .* hidden;
-  eta = 0.1;
 
   dmean = compute_dmean(kernel, images);
   [N, D, K] = size(dmean);
   common3d = reshape(common, N, 1, K);
-  means = eta * reshape(sum(bsxfun(@times, common3d, dmean)), D, K);
+  means = etaM * reshape(sum(bsxfun(@times, common3d, dmean)), D, K);
 
   dvar = compute_dvar(kernel, images);
-  vars = eta * sum(common .* dvar);
+  vars = etaV * sum(common .* dvar);
 
   delta.means = means';
   delta.vars = vars';
--- code_2009-11744/run_rbfn.m
+++ code_2009-11744/run_rbfn.m
@@ -1,18 +1,30 @@
 % 2009-11744 심규민
 
+start = tic;
+
 path = "../data/MNIST_Dataset.mat";
 [training, testing] = load_data(path);
 kernel = make_kernel(training.images, training.labels);
 
-while true
+etaM = 0.3;
+etaV = 0.1;
+
+fprintf("etaM = %.3f, etaV = %.3f\n", etaM, etaV);
+
+for epoch = 1 : 999
   hidden = compute_hidden(kernel, training.images);
   params = fit_parameter(hidden, training.labels);
 
   results = guess(params, kernel, testing.images);
-  success = success_rate(results, testing.labels)
+  testingSuc = success_rate(results, testing.labels);
 
   results = guess(params, kernel, training.images);
-  delta = get_delta(results, params, hidden, kernel, training.images, training.labels);
+  trainingSuc = success_rate(results, training.labels);
+
+  now = toc(start);
+  fprintf("[%03d] testingSuc = %.3f%%, trainingSuc = %.3f%%, elapsed = %.1fs\n", epoch, testingSuc * 100, trainingSuc * 100, now);
+
+  delta = get_delta(etaM, etaV, results, params, hidden, kernel, training.images, training.labels);
   kernel.means += delta.means;
   kernel.vars += delta.vars;
 end

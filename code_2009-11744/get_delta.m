971f42a4ecdd9a2e0db7ce97e5626fddb441c449
Gyumin Sim <sim0629@gmail.com>
2015-10-08 15:17:16 +0900

    Resolve out of memory

--- code_2009-11744/get_delta.m
+++ code_2009-11744/get_delta.m
@@ -3,14 +3,22 @@ function [delta] = get_delta(etaM, etaV, results, params, hidden, kernel, images
   w = reset_bias(params);
   common = ((y - results) * w') .* hidden;
 
-  dmean = compute_dmean(kernel, images);
-  [N, D, K] = size(dmean);
-  common3d = reshape(common, N, 1, K);
-  means = etaM * reshape(sum(bsxfun(@times, common3d, dmean)), D, K);
+  K = length(kernel.vars);
+  [~, D] = size(images);
+  means = zeros(K, D);
+  vars = zeros(K, 1);
 
-  dvar = compute_dvar(kernel, images);
-  vars = etaV * sum(common .* dvar);
+  for k = 1 : K
+    meanK = kernel.means(k, :);
+    varK = kernel.vars(k);
+    diffK = bsxfun(@minus, images, meanK);
+    dmeanK = diffK ./ varK;
+    dvarK = sum(diffK .^ 2, 2) ./ (varK ^ 1.5);
+    commonK = common(:, k);
+    means(k, :) = sum(bsxfun(@times, commonK, dmeanK));
+    vars(k, :) = sum(common(:, k) .* dvarK);
+  end
 
-  delta.means = means';
-  delta.vars = vars';
+  delta.means = etaM * means;
+  delta.vars = etaV * vars;
 end
